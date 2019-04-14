fc38d773f4412c9e076355cb6ba162238531fa7a
Letícia Bernardo <leticiaellenbernardo@gmail.com>
2019-04-13 22:10:22 -0300

    Add file user info config

---
+++ config-dist.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Instagram Access and Configurations
+"""
+INSTA_USERNAME = ""
+INSTA_PASSWORD = ""
+INSTA_TAGS_FOLLOW = ['amazing', 'beautiful', 'adventure']
+INSTA_UNFOLLOW_DISABLED = ['leh.ellen']
+INSTA_COPY_FOLLOWERS_BY = ['leh.ellen']
+
+
+"""
+Facebook Access and Configurations
+"""
+FACEBOOK_USERNAME = ""
+FACEBOOK_PASSWORD = ""
